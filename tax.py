dca1df0459450bb546d6bd800994de7e3e136109
Marek Jędrzejewski <kontakt@marekjedrzejewski.pl>
2020-08-14 18:22:43 +0200

    Add bonus challenge solution
    Naive one, but works fase enought

--- tax.py
+++ tax.py
@@ -1,18 +1,19 @@
 import json
 
 
-class Tax():
+class Tax:
     def __init__(self, filename='default_brackets.json'):
         file = open(filename)
         self.brackets = json.load(file)
         file.close()
 
     def tax(self, amount):
+        """Return tax based on amount of money earned."""
         total_tax = 0
         amount_to_tax = amount
         previous_cap = 0
         for b in self.brackets:
-            if b['cap'] and amount_to_tax > b['cap']:
+            if b['cap'] and amount > b['cap']:
                 current_bracket = b['cap'] - previous_cap
                 total_tax += current_bracket * b['rate']
                 amount_to_tax -= current_bracket
@@ -21,3 +22,12 @@ class Tax():
                 total_tax += amount_to_tax * b['rate']
                 break
         return int(total_tax)
+
+    def overall_rate(self, rate):
+        """Return amount of money needed to get overall tax on given level"""
+        if rate >= self.brackets[-1]['rate']:
+            return None  # No amount can give pure maximum rate or more than it
+        amount = 1
+        while self.tax(amount) / amount < rate:
+            amount += 1
+        return amount
--- tests.py
+++ tests.py
@@ -13,6 +13,15 @@ class TestTax(unittest.TestCase):
         self.assertEqual(t.tax(56789), 8697)
         self.assertEqual(t.tax(1234567), 473326)
 
+    def test_default_overall_rate(self):
+        t = Tax()
+        self.assertGreaterEqual(t.overall_rate(0), 0)
+        self.assertLessEqual(t.overall_rate(0), 10009)
+        self.assertEqual(t.overall_rate(0.06), 25000)
+        self.assertEqual(t.overall_rate(0.09), 34376)
+        self.assertEqual(t.overall_rate(0.32), 256250)
+        self.assertEqual(t.overall_rate(0.40), None)
+
 
 if __name__ == '__main__':
     unittest.main()
